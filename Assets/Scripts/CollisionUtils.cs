2a4bc807ee4ec5c0a3fbccbc3bb03d37d3696cdc
Dan Quill <admin@danjb.com>
2021-04-26 18:04:40 +0100

    Implement non-traversable slopes and refactor
    Note that while this is works, it is not perfect; when transitioning between the ground and a slope, entities become
    "stuck" briefly, and we have to rely on the unstuck logic to free them.
    
    This is a consequence of the way we handle multiple collisions occurring in a single frame. Since we process them
    sequentially and the entity gets moved after each collision, subsequent collision results get skewed because we are
    no longer simulating the exact movement that the entity has taken.

---
+++ Assets/Scripts/CollisionUtils.cs
@@ -0,0 +1,223 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CollisionUtils
+{
+
+  /**
+   * Determines if a collision is valid for the given collision node.
+   *
+   * `node` is a position relative to the center of the hitbox, and `extents`
+   * is the distance from the center of that hitbox to its edges.
+   */
+  public static bool CanCollide(
+      Vector2 node, RaycastHit2D hit, Vector2 extents, float slopeTolerance)
+  {
+    if (IsCollisionWithFloor(hit, slopeTolerance))
+    {
+      // Only nodes at the bottom of the entity can collide with floors
+      return node.y == -extents.y;
+    }
+    else if (IsCollisionWithCeiling(hit))
+    {
+      // Only nodes at the top of the entity can collide with ceilings
+      return node.y == extents.y;
+    }
+    else if (IsCollisionWithRightWall(hit))
+    {
+      // Only nodes at the right of the entity can collide with right walls
+      return node.x == extents.x;
+    }
+    else if (IsCollisionWithLeftWall(hit))
+    {
+      // Only nodes at the left of the entity can collide with left walls
+      return node.x == -extents.x;
+    }
+    else
+    {
+      // We have collided with a non-traversable slope. For now, since our scene
+      // contains only sloped floors (not sloped ceilings), only allow nodes at
+      // the bottom of the entity to collide.
+      return node.y == -extents.y;
+    }
+  }
+
+  /**
+   * Calculates the movement vector to apply in order to escape a solid in which
+   * we have become embedded, given the collision that should have occurred, the
+   * previous position and the current (embedded) position.
+   *
+   * The resulting vector is in the direction of the collision normal. This
+   * ensures that we preserve as much of the original movement as possible. For
+   * example, if we hit a floor while moving diagonally, we still want the
+   * horizontal motion to be applied.
+   */
+  public static Vector2 CalculateCollisionEscapeVector(
+      Vector2 currentPosition,
+      Vector2 prevPosition,
+      RaycastHit2D hit,
+      float slopeTolerance)
+  {
+    // The journey we have just taken that has left us embedded in a solid
+    Vector2 journey = currentPosition - prevPosition;
+
+    // Distance travelled inside the solid (after the collision occurred)
+    float distanceTravelledInSolid = journey.magnitude - hit.distance;
+
+    // The portion of the journey for which we were embedded in the solid
+    Vector2 journeyInSolid = journey.normalized * distanceTravelledInSolid;
+
+    Vector2 escapeVector;
+
+    if (IsCollisionWithSlope(hit))
+    {
+      // We have collided with a sloped floor. We need to treat this as a
+      // special case, because if we moved the entity in the direction of the
+      // normal then they would slide down the slope (which we don't want).
+      escapeVector = CalculateCollisionEscapeVectorForSlope(
+          journeyInSolid, hit, slopeTolerance);
+    }
+    else
+    {
+      // Negating `journeyInSolid` gives us the journey we would need to take to
+      // get back OUT of the solid. However, we don't want to just retrace our
+      // steps - we want to escape in the direction of the collision normal - so
+      // we have to project this outward journey onto the normal.
+      escapeVector = Vector3.Project(-journeyInSolid, hit.normal);
+    }
+
+    // Add a tiny bit extra, just to make sure we are clear of the solid
+    return escapeVector
+        + escapeVector.normalized * EntityPhysics.SmallestDistance;
+  }
+
+  /**
+   * Given a journey we have just taken that has embedded us in a slope, and the
+   * collision with that slope, calculates the movement vector to apply in order
+   * to position this entity atop the slope.
+   */
+  public static Vector2 CalculateCollisionEscapeVectorForSlope(
+      Vector2 journeyInSlope, RaycastHit2D hit, float slopeTolerance)
+  {
+    if (IsCollisionWithFloor(hit, slopeTolerance))
+    {
+      return CalculateCollisionEscapeVectorForTraversableSlope(
+          journeyInSlope, hit);
+    }
+    else
+    {
+      Vector2 vec = CalculateCollisionEscapeVectorForNonTraversableSlope(
+          journeyInSlope, hit);
+      return vec;
+    }
+  }
+
+  public static Vector2 CalculateCollisionEscapeVectorForTraversableSlope(
+      Vector2 journeyInSlope, RaycastHit2D hit)
+  {
+    // We want to move directly up, so that we are sitting atop the slope. This
+    // means we need to calculate the vertical distance between the endpoint of
+    // our journey and the point directly above it that lies on the surface of
+    // the slope.
+
+    // First we need a vector representing the slope itself
+    Vector2 slope = Vector2.Perpendicular(hit.normal);
+
+    // Flip this vector if necessary, so that it's pointing in the direction of
+    // horizontal travel.
+    if (Mathf.Sign(slope.x) != Mathf.Sign(journeyInSlope.x))
+    {
+      slope = -slope;
+    }
+
+    // To find the desired point on the slope, we need to scale our slope vector
+    // until it covers the same x-distance as `journeyInSlope`.
+    Vector2 scaledSlope = VectorUtils.ScaleToWidth(slope, journeyInSlope.x);
+
+    // We want a vector that connects the endpoint of our journey to the
+    // endpoint of this `scaledSlope` vector. This should point straight up.
+    return scaledSlope - journeyInSlope;
+  }
+
+  public static Vector2 CalculateCollisionEscapeVectorForNonTraversableSlope(
+      Vector2 journeyInSlope, RaycastHit2D hit)
+  {
+    // This is similar to the traversable case, but instead of moving up we want
+    // to move directly sideways until we are sitting atop the slope. This
+    // ensures that we can never ascend the slope, because any vertical progress
+    // is effectively disgarded.
+
+    // First we need a vector representing the slope itself
+    Vector2 slope = Vector2.Perpendicular(hit.normal);
+
+    // Flip this vector if necessary, so that it's pointing in the OPPOSITE
+    // direction of horizontal travel.
+    if (Mathf.Sign(slope.x) == Mathf.Sign(journeyInSlope.x))
+    {
+      slope = -slope;
+    }
+
+    // To find the desired point on the slope, we need to scale our slope vector
+    // until it covers the same y-distance as `journeyInSlope`.
+    Vector2 scaledSlope = VectorUtils.ScaleToHeight(slope, journeyInSlope.y);
+
+    // We want a vector that connects the endpoint of our journey to the
+    // endpoint of this `scaledSlope` vector. This should point horizontally.
+    return scaledSlope - journeyInSlope;
+  }
+
+  /**
+   * Determines if a collision is with a sloped floor.
+   */
+  public static bool IsCollisionWithSlope(RaycastHit2D hit)
+  {
+    return hit.normal != Vector2.up && hit.normal.y > 0;
+  }
+
+  /**
+   * Determines if a collision is with a floor (including sloped floors, as
+   * long as they are traversable).
+   */
+  public static bool IsCollisionWithFloor(
+      RaycastHit2D hit, float slopeTolerance)
+  {
+    return hit.normal.y > slopeTolerance;
+  }
+
+  /**
+   * Determines if a collision is with a ceiling.
+   */
+  public static bool IsCollisionWithCeiling(RaycastHit2D hit)
+  {
+    return hit.normal == Vector2.down;
+  }
+
+  /**
+   * Determines if a collision is with a wall.
+   */
+  public static bool IsCollisionWithWall(RaycastHit2D hit, float slopeTolerance)
+  {
+    return IsCollisionWithLeftWall(hit) || IsCollisionWithRightWall(hit)
+        // Steep slopes are treated as walls for collision purposes
+        || (IsCollisionWithSlope(hit)
+            && !IsCollisionWithFloor(hit, slopeTolerance));
+  }
+
+  /**
+   * Determines if a collision is with a left wall.
+   */
+  public static bool IsCollisionWithLeftWall(RaycastHit2D hit)
+  {
+    return hit.normal == Vector2.right;
+  }
+
+  /**
+   * Determines if a collision is with a right wall.
+   */
+  public static bool IsCollisionWithRightWall(RaycastHit2D hit)
+  {
+    return hit.normal == Vector2.left;
+  }
+
+}
--- Assets/Scripts/EntityPhysics.cs
+++ Assets/Scripts/EntityPhysics.cs
@@ -12,6 +12,14 @@ public class EntityPhysics : MonoBehaviour
 {
 
   /**
+   * The smallest possible distance that matters, as far as our physics are
+   * concerned.
+   *
+   * Distances smaller than this can safely be rounded to zero.
+   */
+  public const float SmallestDistance = 0.0001f;
+
+  /**
    * Change in vertical velocity to apply each tick.
    */
   private const float Gravity = -1f;
@@ -29,6 +37,14 @@ public class EntityPhysics : MonoBehaviour
   private const float GroundFriction = 1.5f;
 
   /**
+   * Minimum y-component of the normal of a slope in order for it to be
+   * traversable.
+   *
+   * Steeper slopes become traversable as this approaches zero.
+   */
+  private const float SlopeTolerance = 0.75f;
+
+  /**
    * Maximum magnitude of an entity's x-velocity (m/s).
    *
    * If this is set too high, entities may pass through thin level geometry.
@@ -41,14 +57,6 @@ public class EntityPhysics : MonoBehaviour
    * If this is set too high, entities may pass through thin level geometry.
    */
   private const float TerminalVelocity = -14f;
-
-  /**
-   * The smallest possible distance that matters, as far as our physics are
-   * concerned.
-   *
-   * Distances smaller than this can safely be rounded to zero.
-   */
-  private const float SmallestDistance = 0.0001f;
 
   /**
    * How far a "stuck" entity should move, in the hope of becoming unstuck.
@@ -140,7 +148,6 @@ public class EntityPhysics : MonoBehaviour
     ApplyJump();
     ApplyGravity();
     ApplyFriction();
-    RememberPosition();
     ScheduleMove();
   }
 
@@ -229,6 +236,9 @@ public class EntityPhysics : MonoBehaviour
    */
   private void ScheduleMove()
   {
+    // Remember our position before any movement is applied
+    prevPosition = transform.position;
+
     // Reset this flag to prepare for imminent collisions
     grounded = false;
 
@@ -280,7 +290,8 @@ public class EntityPhysics : MonoBehaviour
 
       // We only care about collisions with the collider that caused this
       // callback; other colliders can take care of their own collisions.
-      if (hit.collider == other && CanCollide(node, hit))
+      if (hit.collider == other && CollisionUtils.CanCollide(
+          node, hit, spriteRenderer.bounds.extents, SlopeTolerance))
       {
         ProcessCollision(hit);
         return;
@@ -289,163 +300,44 @@ public class EntityPhysics : MonoBehaviour
   }
 
   /**
-   * Determines if a collision is valid for the given collision node.
-   */
-  private bool CanCollide(Vector2 node, RaycastHit2D hit)
-  {
-
-    if (hit.normal == Vector2.up)
-    {
-      // Only nodes at the bottom of the entity can collide with floors
-      return node.y == -spriteRenderer.bounds.extents.y;
-    }
-    else if (hit.normal == Vector2.down)
-    {
-      // Only nodes at the top of the entity can collide with ceilings
-      return node.y == spriteRenderer.bounds.extents.y;
-    }
-    else if (hit.normal == Vector2.left)
-    {
-      // Only nodes at the right of the entity can collide with right walls
-      return node.x == spriteRenderer.bounds.extents.x;
-    }
-    else if (hit.normal == Vector2.right)
-    {
-      // Only nodes at the left of the entity can collide with left walls
-      return node.x == -spriteRenderer.bounds.extents.x;
-    }
-    // We have collided with a slope. For now, just allow all nodes to collide
-    // with slopes. Later, we could be smarter about this by considering the
-    // angle of the slope.
-    return true;
-  }
-
-  /**
    * Moves the entity away from the given collision until it is no longer
    * colliding, and changes the velocity as appropriate.
    */
   private void ProcessCollision(RaycastHit2D hit)
   {
-    // Calculate how far we have become embedded in the solid
-    Vector2 escapeVector = CalculateCollisionEscapeVector(hit);
+    if (hit.distance == 0)
+    {
+      // We are already inside the solid, so we can't rely on the collision
+      // normal. If this happens, something has gone wrong. For now, just try
+      // moving up and "backwards" and hope that we escape the collision.
+      rigidbody.transform.position += (Vector3)Vector2.up * UnstuckDistance
+          + (Vector3)hit.normal * UnstuckDistance;
+      rigidbody.velocity = VectorUtils.SetY(rigidbody.velocity, 0);
+      return;
+    }
+
+    // Figure out how far we need to move to escape the solid in which we are
+    // embedded.
+    Vector2 escapeVector = CollisionUtils.CalculateCollisionEscapeVector(
+        rigidbody.position, prevPosition, hit, SlopeTolerance);
 
     // Move away from the collision
     rigidbody.transform.position += (Vector3)escapeVector;
 
     // Adjust velocity
-    if (hit.normal == Vector2.up || hit.normal.y > 0)
+    if (CollisionUtils.IsCollisionWithFloor(hit, SlopeTolerance))
     {
-      // Floors / slopes
       rigidbody.velocity = VectorUtils.SetY(rigidbody.velocity, 0);
       grounded = true;
     }
-    else if (hit.normal == Vector2.down)
+    else if (CollisionUtils.IsCollisionWithCeiling(hit))
     {
-      // Ceilings
       rigidbody.velocity = VectorUtils.SetY(rigidbody.velocity, 0);
     }
-    else if (hit.normal == Vector2.left || hit.normal == Vector2.right)
+    else if (CollisionUtils.IsCollisionWithWall(hit, SlopeTolerance))
     {
-      // Walls
       rigidbody.velocity = VectorUtils.SetX(rigidbody.velocity, 0);
     }
-  }
-
-  /**
-   * Calculates the movement vector to apply in order to escape a solid in which
-   * we have become embedded, given the collision that should have occurred and
-   * knowing where we are now.
-   *
-   * The resulting vector is in the direction of the collision normal. This
-   * ensures that we preserve as much of the original movement as possible. For
-   * example, if we hit a floor while moving diagonally, we still want the
-   * horizontal motion to be applied.
-   */
-  private Vector2 CalculateCollisionEscapeVector(RaycastHit2D hit)
-  {
-    if (hit.distance == 0)
-    {
-      // We are already inside the solid, so we can't rely on the collision
-      // normal. If this happens, something has gone wrong. For now, just try
-      // moving up and hope that we escape the collision.
-      return Vector2.up * UnstuckDistance;
-    }
-
-    // The journey we have just taken that has left us embedded in a solid
-    Vector2 journey = rigidbody.position - prevPosition;
-
-    // Distance travelled inside the solid (after the collision occurred)
-    float distanceTravelledInSolid = journey.magnitude - hit.distance;
-
-    // The portion of the journey for which we were embedded in the solid
-    Vector2 journeyInSolid = journey.normalized * distanceTravelledInSolid;
-
-    if (ShouldClimbSlope(hit))
-    {
-      // We have collided with a sloped floor. We need to treat this as a
-      // special case, because if we moved the entity in the direction of the
-      // normal then they would slide down the slope (which we don't want).
-      return CalculateCollisionEscapeVectorForSlope(journeyInSolid, hit);
-    }
-
-    // Negating `journeyInSolid` gives us the journey we would need to take to
-    // get back OUT of the solid. However, we don't want to just retrace our
-    // steps - we want to escape in the direction of the collision normal - so
-    // we have to project this outward journey onto the normal.
-    Vector2 escapeVector = Vector3.Project(-journeyInSolid, hit.normal);
-
-    // We add a tiny bit extra, just to make sure we are clear of the solid
-    return escapeVector + hit.normal * SmallestDistance;
-  }
-
-  /**
-   * Given a journey we have just taken that has embedded us in a slope, and the
-   * collision with that slope, calculates the movement vector to apply in order
-   * to position this entity atop the slope.
-   */
-  private Vector2 CalculateCollisionEscapeVectorForSlope(
-      Vector2 journeyInSlope, RaycastHit2D hit)
-  {
-    // We want to move directly up, so that we are sitting atop the slope. This
-    // means we need to calculate the vertical distance between the endpoint of
-    // our journey and the point directly above it that lies on the surface of
-    // the slope. First we need a vector representing the slope itself
-    Vector2 slope = Vector2.Perpendicular(hit.normal);
-
-    // Flip this vector if necessary, so that it's pointing in the direction of
-    // horizontal travel.
-    if (Mathf.Sign(slope.x) != Mathf.Sign(journeyInSlope.x))
-    {
-      slope = -slope;
-    }
-
-    // To find the desired point on the slope, we need to scale our slope vector
-    // until it covers the same x-distance as `journeyInSlope`.
-    Vector2 scaledSlope = VectorUtils.ScaleToWidth(slope, journeyInSlope.x);
-
-    // We want a vector that connects the endpoint of our journey to the
-    // endpoint of this `scaledSlope` vector. This should point straight up.
-    Vector2 escapeVector = scaledSlope - journeyInSlope;
-
-    // We add a tiny bit extra, just to make sure we are clear of the slope
-    return escapeVector + Vector2.up * SmallestDistance;
-  }
-
-  /**
-   * Given a collision with a slope, determines if it should be ascended.
-   */
-  private bool ShouldClimbSlope(RaycastHit2D hit)
-  {
-    // For now, all slopes can be climbed
-    return hit.normal != Vector2.up && hit.normal.y > 0;
-  }
-
-  /**
-   * Store the current position for use in any imminent collisions.
-   */
-  private void RememberPosition()
-  {
-    prevPosition = transform.position;
   }
 
 }
